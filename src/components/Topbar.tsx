240f457e3548f453d2dbba504f7c7dc240433747
luiscacl <luiscaslop1@gmail.com>
2025-03-11 14:11:10 -0600

    Fix Spotify Imgs

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -12,7 +12,11 @@ const Topbar = () => {
     "
     >
       <div className="flex gap-2 items-center">
-        <img src="/spotify.png" className="size-8" alt="Spotify logo" />
+        <img
+          src="../../public/spotify.png"
+          className="size-8"
+          alt="Spotify logo"
+        />
         Spotify
       </div>
       <div className="flex items-center gap-4">
